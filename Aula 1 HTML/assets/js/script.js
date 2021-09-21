bc956ba3fd55b58d49258a4c14756e805ec04747
Beatriz <biiadiiego@gmail.com>
2021-09-21 15:13:48 -0300

    [Adicionando JavaScript ao meu site]

---
+++ Aula 1 HTML/assets/js/script.js
@@ -0,0 +1,76 @@
+/*
+Case sensitive = reconhece maiúsculas e minúsculas.
+
+por Tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getElementByName()
+por Classe: getElementByClassName()
+por Seletor: querySelector()
+
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
+
+nome.style.width = '100%'
+email.style.width = '100%' 
+
+function validaNome() {
+    let txtNome = document.querySelector('#txtNome')
+    if (nome.value.length < 3) {
+        txtNome.innerHTML = 'Nome inválido'
+        txtNome.style.color = 'red'
+    } else {
+        txtNome.innerHTML = 'Nome válido'
+        txtNome.style.color = 'green'
+        nomeOk = true
+    }
+}
+
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){
+        txtEmail.innerHTML = 'E-mail inválido'
+        txtEmail.style.color = 'red'
+    } else {
+        txtEmail.innerHTML = 'E-mail válido'
+        txtEmail.style.color = 'green'
+        emailOk = true
+    }
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+    if (assunto.value.length >= 400){
+        txtAssunto.innerHTML = 'Você escreve muito! Digite menos de 400 caracteres.'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    } else {
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+
+function enviar(){
+    if (nomeOk == true && emailOk == true && assuntoOk == true){
+        alert ('Formulário enviado com sucesso!')
+    } else {
+        alert ('Preencha o formulário corretamente antes de enviar.')
+        alert.style.color = 'red'
+    }
+}
+
+function mapaZoom(){
+    mapa.style.width = '800px'
+    mapa.style.height = '600px'
+}
+
+function mapaNormal(){
+    mapa.style.width = '400px'
+    mapa.style.height = '250px'
+}
